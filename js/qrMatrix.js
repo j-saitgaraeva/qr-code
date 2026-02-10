59ca8c7736fbe1e1d50abd6a63aac98e20cb992c
j-saitgaraeva <121194088+j-saitgaraeva@users.noreply.github.com>
2026-02-10 18:00:52 +0300

    Update qrMatrix.js
    Другая библиотека

--- js/qrMatrix.js
+++ js/qrMatrix.js
@@ -1,18 +1,4 @@
 export function generateMatrix(text) {
-    const qr = qrcode(0, "H");
-    qr.addData(text);
-    qr.make();
-
-    const size = qr.getModuleCount();
-    const matrix = [];
-
-    for (let r = 0; r < size; r++) {
-        const row = [];
-        for (let c = 0; c < size; c++) {
-            row.push(qr.isDark(r, c) ? 1 : 0);
-        }
-        matrix.push(row);
-    }
-
-    return matrix;
+    const qr = QRCode.generate(text);
+    return qr.modules;
 }
