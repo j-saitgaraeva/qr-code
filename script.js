85b993bda0d3b9a588fd7384ed4f704a24fcac48
j-saitgaraeva <121194088+j-saitgaraeva@users.noreply.github.com>
2026-02-18 10:35:57 +0300

    Update script.js
    Одинаковый стиль для рамки и глазков

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ const qrCode = new QRCodeStyling({
      * Здесь ставим extra-rounded (или rounded, если extra-rounded не сработает).
      * Это создаёт рамку с сильным радиусом, похожим на твой SVG.
      */
-    type: "extra-rounded",
+    type: "classy-rounded", // было "extra-rounded"
     color: "#000000"
   },
   // Центр глазков — скруглённый квадрат
   cornersDotOptions: {
-    type: "extra-rounded", // было "rounded", "square", "classy", "classy-rounded"
+    type: "classy-rounded", // было "rounded", "square", "classy", "classy-rounded", "extra-rounded"
     color: "#000000"
   }
 });
